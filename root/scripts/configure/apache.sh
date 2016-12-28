ad438060c797ef7818de750e7e10c79eca9aea79
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-12-28 23:38:31 +0100

    create pid file dir

--- root/scripts/configure/apache.sh
+++ root/scripts/configure/apache.sh
@@ -3,6 +3,7 @@
 # configure
 mkdir -p "${APACHE_WEB_ROOT}" && chown -R apache:www-data "${APACHE_WEB_ROOT}"
 mkdir -p "${APACHE_RUN}"      && chown -R apache:www-data "${APACHE_RUN}"
+mkdir -p "${APACHE_PIDF%/*}"  && chown -R apache:www-data "${APACHE_PIDF%/*}"
 
 IP_ADDR=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 sed -i "s@\(#ServerName\).*@\ServerName ${IP_ADDR}:80@g" "${APACHE_CONF}"
