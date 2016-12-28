62b5afcca03f381b24c4aaa8adaf27021575e238
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-12-29 00:36:52 +0100

    remove old pid files on reboot

--- root/scripts/start/apache.sh
+++ root/scripts/start/apache.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # Start the apache2 web server
 
-# delete old PID file
+# delete old PID file(s)
 if [ -f "${APACHE_PIDF}" ]; then
-    rm "${APACHE_PIDF}"
+    rm "${APACHE_PIDF%/*}"/* -rf
 fi
 
 httpd "${@}"
